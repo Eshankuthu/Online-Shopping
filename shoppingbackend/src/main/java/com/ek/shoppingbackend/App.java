d8c5e635811a15d9598b7456cee2a04c7edf7184
eshankuthu <kuthu.eshan@gmail.com>
2017-08-06 22:51:40 -0500

    My Project Setup Completed Succesfully

---
+++ shoppingbackend/src/main/java/com/ek/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.ek.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
